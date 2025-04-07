45fdb0b1a3010fc08388d3796e075bf2b1bed0f9
9fourlabs <tbrown@9fourlabs.com>
2025-04-07 10:59:39 +0000

    personalization updates

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -116,37 +116,6 @@ export const Contact = (props) => {
                 {props.data ? props.data.phone : "loading"}
               </p>
             </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-envelope-o"></i> Email
-                </span>{" "}
-                {props.data ? props.data.email : "loading"}
-              </p>
-            </div>
-          </div>
-          <div className="col-md-12">
-            <div className="row">
-              <div className="social">
-                <ul>
-                  <li>
-                    <a href={props.data ? props.data.facebook : "/"}>
-                      <i className="fa fa-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.twitter : "/"}>
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
           </div>
         </div>
       </div>
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -10,13 +10,13 @@ export const Services = (props) => {
             At 5.J. Construction, we offer a comprehensive range of concrete-related services in Covington, Georgia, and the surrounding areas. We are dedicated to providing affordable and high-quality solutions for all your concrete needs, from residential driveways and patios to commercial foundations and more. Our experienced team ensures durable and reliable results on every project.
           </p>
         </div>
-        <div className="row">
+        <div className="row row-cols-1 row-cols-md-3 g-4"> {/* Added Bootstrap grid classes */}
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4">
+                <div key={`${d.name}-${i}`} className="col"> {/* Changed to col */}
                   {" "}
                   <i className={d.icon}></i>
-                  <div className="service-desc">
+                  <div className="service-desc h-100"> {/* Added h-100 */}
                     <h3>{d.name}</h3>
                     <p>{d.text}</p>
                   </div>
